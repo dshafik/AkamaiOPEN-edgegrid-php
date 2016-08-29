fce63720993221ce28946d461133204ec0cf3bfb
Davey Shafik <dshafik@akamai.com>
2016-08-29 00:22:11 -0700

    Allow testing on PHP 5.3/5.4

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -19,4 +19,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-require_once __DIR__ . '/../vendor/autoload.php';
+if (\version_compare(PHP_VERSION, '5.5.0', '>=')) {
+    require_once __DIR__ . '/../vendor/autoload.php';
+}
